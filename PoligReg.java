5d3292b3f9bd97f4f7dda3a4680d8aeae3929882
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2024-03-11 07:08:54 -0600

    ya genera un poligono regular
    Genera un poligono regular de n vertices al azar entre 3 a 10

--- PoligReg.java
+++ PoligReg.java
@@ -3,27 +3,34 @@ import java.awt.*;
 
 public class PoligReg extends JPanel{
 	//Variables que vamo a usar
+	private int lados,radio=100;
 	private Coordenada[] coordenada;
 	private Polygon poligono = new Polygon();	
 
 	//Constructor de la clase
 	public PoligReg(int n){
-		Coordenada[] coordenada=new Coordenada[n];
-		coordenada[0]=new Coordenada(0,0);
-		coordenada[1]=new Coordenada(100,0);
-		coordenada[2]=new Coordenada(100,100);
-		coordenada[3]=new Coordenada(0,100);
+
+		//Pa' que genere poligonos regulare de 3 a 10 vertices y radio
+		this.lados = (int)(Math.random()*10+3);
+		this.radio = (int)(Math.random()*120+10);
+
+		Coordenada[] coordenada= new Coordenada[lados];
+		//Para guardar las coordenadas
+		for(int i=0;i<lados;i++){
+			double angulo=2*Math.PI*i/lados;
+			coordenada[i]=new Coordenada(400+radio * Math.cos(angulo), 300+radio * Math.sin(angulo));
+		}
 
 		Timer timer = new Timer(50, e -> {
 			poligono.reset();
 
-			//Con esto ponemos los puntos
-			for(int i=0;i<n;i++){
+			//Con esto ponemos los puntos a nuestro poligono
+			for(int i=0;i<lados;i++){
 				poligono.addPoint((int)coordenada[i].abcisa(),(int)coordenada[i].ordenada());
 			}
 
 			// Incrementa las coordenadas
-			for(int i=0;i<n;i++){
+			for(int i=0;i<lados;i++){
 				coordenada[i]=new Coordenada(coordenada[i].abcisa()+1,coordenada[i].ordenada()+1);
 			}
 
--- inicio.java
+++ inicio.java
@@ -8,7 +8,7 @@ public class inicio extends JPanel{
 			JFrame frame = new JFrame("Proyecto 2");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setSize(800,600);
-			frame.add(new PoligReg(4));
+			frame.add(new PoligReg(3));
 			frame.setVisible(true);
 		});
 
