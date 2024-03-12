f4885a61bc0ddbb97efdd2e5e1932d5eb7710e2e
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2024-03-11 21:24:38 -0600

    Add files via upload

--- PoligReg.java
+++ PoligReg.java
@@ -1,49 +1,68 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class PoligReg extends JPanel{
-	//Variables que vamo a usar
-	private int lados,radio=100;
-	private Coordenada[] coordenada;
-	private Polygon poligono = new Polygon();	
+public class PoligReg extends JPanel {
+    private int lados, radio, centroX, centroY, n;
+    private Coordenada[][] coordenadas;
+    private Polygon[] poligonos;
 
-	//Constructor de la clase
-	public PoligReg(int n){
+    public PoligReg(int n) {
+        this.n = n;
+        this.lados = (int) (Math.random() * 10 + 3);
+        this.radio = (int) (Math.random() * 100 + 10);
 
-		//Pa' que genere poligonos regulare de 3 a 10 vertices y radio
-		this.lados = (int)(Math.random()*10+3);
-		this.radio = (int)(Math.random()*120+10);
+        // Inicializa los arrays
+        this.coordenadas = new Coordenada[n][lados];
+        this.poligonos = new Polygon[n];
 
-		Coordenada[] coordenada= new Coordenada[lados];
-		//Para guardar las coordenadas
-		for(int i=0;i<lados;i++){
-			double angulo=2*Math.PI*i/lados;
-			coordenada[i]=new Coordenada(400+radio * Math.cos(angulo), 300+radio * Math.sin(angulo));
-		}
+        // Inicializa cada polígono y sus coordenadas
+        for (int j = 0; j < n; j++) {
+	    centroX = (int) (Math.random() * 650 + 1);
+            centroY = (int) (Math.random() * 450 + 1);
 
-		Timer timer = new Timer(50, e -> {
-			poligono.reset();
+            poligonos[j] = new Polygon();
 
-			//Con esto ponemos los puntos a nuestro poligono
-			for(int i=0;i<lados;i++){
-				poligono.addPoint((int)coordenada[i].abcisa(),(int)coordenada[i].ordenada());
-			}
+            for (int i = 0; i < lados; i++) {
+                double angulo = 2 * Math.PI * i / lados;
+                coordenadas[j][i] = new Coordenada(
+                        centroX + radio * Math.cos(angulo) + j * 30, // Añadido un desplazamiento en X
+                        centroY + radio * Math.sin(angulo)
+                );
+            }
+        }
 
-			// Incrementa las coordenadas
-			for(int i=0;i<lados;i++){
-				coordenada[i]=new Coordenada(coordenada[i].abcisa()+1,coordenada[i].ordenada()+1);
-			}
+        Timer timer = new Timer(50, e -> {
+            // Actualiza las coordenadas de cada polígono
+            for (int j = 0; j < n; j++) {
+                for (int i = 0; i < lados; i++) {
+                    coordenadas[j][i] = new Coordenada(
+                            coordenadas[j][i].abcisa() + 1,
+                            coordenadas[j][i].ordenada() + 1
+                    );
+                }
+            }
 
-			// Vuelve a dibujar
-			repaint();
-        	});
-		timer.start();
-	}
+            // Vuelve a dibujar
+            repaint();
+        });
+        timer.start();
+    }
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawPolygon(poligono);
-	}
-} 
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Dibuja todos los polígonos
+        for (int j = 0; j < n; j++) {
+            poligonos[j].reset();
+
+            for (int i = 0; i < lados; i++) {
+                poligonos[j].addPoint((int) coordenadas[j][i].abcisa(), (int) coordenadas[j][i].ordenada());
+            }
+
+            g2d.drawPolygon(poligonos[j]);
+        }
+    }
+}
+
